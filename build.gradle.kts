0df41c03525a8d6755ee1a10be442a00da966b9b
Null_Codes <20952594+Null-Codes@users.noreply.github.com>
2026-07-14 09:13:18 -0500

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
+    id("com.diffplug.spotless") version "8.8.0"
+}
+
+group = "com.null_codes"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    paperweight.paperDevBundle("26.1.2.build.+")
+
+    implementation("org.xerial:sqlite-jdbc:3.46.1.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+tasks.processResources {
+    expand("version" to project.version)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+spotless {
+    java {
+        importOrder()
+        removeUnusedImports()
+        expandWildcardImports()
+        cleanthat().sourceCompatibility("1.25")
+        googleJavaFormat()
+        formatAnnotations()
+    }
+    yaml {
+        target("src/resources/*.yml")
+        jackson()
+    }
+}
---
+++ src/main/java/com/null_codes/hearth/HearthPlugin.java
@@ -0,0 +1,12 @@
+package com.null_codes.hearth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HearthPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+}

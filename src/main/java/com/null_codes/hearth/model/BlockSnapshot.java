07ec88557b300f5833894f2597d99cda81f14480
Null_Codes <20952594+Null-Codes@users.noreply.github.com>
2026-07-14 10:00:25 -0500

    Expand BlockSnapshot to include world

--- src/main/java/com/null_codes/hearth/model/BlockSnapshot.java
+++ src/main/java/com/null_codes/hearth/model/BlockSnapshot.java
@@ -1,12 +1,18 @@
 package com.null_codes.hearth.model;
 
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 
-public record BlockSnapshot(Material material, int x, int y, int z, String blockData) {
+public record BlockSnapshot(Material material, int x, int y, int z, World world, String blockData) {
 
   public BlockSnapshot from(Block block) {
-    return new BlockSnapshot(block.getType(), block.getX(), block.getY(), block.getZ(), block.getBlockData().getAsString());
+    return new BlockSnapshot(
+        block.getType(),
+        block.getX(),
+        block.getY(),
+        block.getZ(),
+        block.getWorld(),
+        block.getBlockData().getAsString());
   }
-
 }

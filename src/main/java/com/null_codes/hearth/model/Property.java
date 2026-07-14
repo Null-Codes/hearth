696721ad99d031a2245e2e280fde120eae7bb5c5
Null_Codes <20952594+Null-Codes@users.noreply.github.com>
2026-07-14 09:45:35 -0500

    Compare worlds in property check

--- src/main/java/com/null_codes/hearth/model/Property.java
+++ src/main/java/com/null_codes/hearth/model/Property.java
@@ -2,12 +2,16 @@ package com.null_codes.hearth.model;
 
 import java.util.UUID;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.util.BoundingBox;
 
 public record Property(
     UUID uuid, UUID owner, String name, UUID world, BoundingBox region, long timestamp) {
 
   public boolean contains(Location location) {
+    World otherWorld = location.getWorld();
+    if (otherWorld == null || !otherWorld.getUID().equals(world)) return false;
     return region.contains(location.toVector());
   }
 }

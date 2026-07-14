011e3a286a5c1b10de028abe42686dc285753902
Null_Codes <20952594+Null-Codes@users.noreply.github.com>
2026-07-14 09:57:13 -0500

    Add from(Block) to capture block snapshots

--- src/main/java/com/null_codes/hearth/model/BlockSnapshot.java
+++ src/main/java/com/null_codes/hearth/model/BlockSnapshot.java
@@ -1,7 +1,12 @@
 package com.null_codes.hearth.model;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 
-import java.util.Map;
+public record BlockSnapshot(Material material, int x, int y, int z, String blockData) {
 
-public record BlockSnapshot(Material material, int x, int y, int z, Map<String, Object> blockData) {}
+  public BlockSnapshot from(Block block) {
+    return new BlockSnapshot(block.getType(), block.getX(), block.getY(), block.getZ(), block.getBlockData().getAsString());
+  }
+
+}

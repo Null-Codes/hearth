7040fb7a0e3efc73b8d1ee4e9a359f4bcc76282c
Null_Codes <20952594+Null-Codes@users.noreply.github.com>
2026-07-15 10:21:02 -0500

    add block snapshot tests

--- src/test/java/com/null_codes/hearth/model/BlockSnapshotTest.java
+++ src/test/java/com/null_codes/hearth/model/BlockSnapshotTest.java
@@ -1,3 +1,73 @@
 package com.null_codes.hearth.model;
 
-public class BlockSnapshotTest {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.bukkit.Material;
+import org.junit.jupiter.api.Test;
+
+class BlockSnapshotTest {
+
+  @Test
+  void storesMaterialCoordinatesAndBlockData() {
+    BlockSnapshot snapshot =
+        new BlockSnapshot(Material.STONE, 10, 64, -20, null, "minecraft:stone");
+
+    assertEquals(Material.STONE, snapshot.material());
+    assertEquals(10, snapshot.x());
+    assertEquals(64, snapshot.y());
+    assertEquals(-20, snapshot.z());
+    assertNull(snapshot.worldUuid());
+    assertEquals("minecraft:stone", snapshot.blockData());
+  }
+
+  @Test
+  void equalSnapshotsAreEqual() {
+    BlockSnapshot first =
+        new BlockSnapshot(Material.OAK_LOG, 5, 70, 12, null, "minecraft:oak_log[axis=y]");
+
+    BlockSnapshot second =
+        new BlockSnapshot(Material.OAK_LOG, 5, 70, 12, null, "minecraft:oak_log[axis=y]");
+
+    assertEquals(first, second);
+    assertEquals(first.hashCode(), second.hashCode());
+  }
+
+  @Test
+  void snapshotsWithDifferentMaterialsAreNotEqual() {
+    BlockSnapshot stone = new BlockSnapshot(Material.STONE, 5, 70, 12, null, "minecraft:stone");
+
+    BlockSnapshot dirt = new BlockSnapshot(Material.DIRT, 5, 70, 12, null, "minecraft:dirt");
+
+    assertNotEquals(stone, dirt);
+  }
+
+  @Test
+  void snapshotsWithDifferentCoordinatesAreNotEqual() {
+    BlockSnapshot first = new BlockSnapshot(Material.STONE, 5, 70, 12, null, "minecraft:stone");
+
+    BlockSnapshot second = new BlockSnapshot(Material.STONE, 6, 70, 12, null, "minecraft:stone");
+
+    assertNotEquals(first, second);
+  }
+
+  @Test
+  void snapshotsWithDifferentBlockDataAreNotEqual() {
+    BlockSnapshot verticalLog =
+        new BlockSnapshot(Material.OAK_LOG, 5, 70, 12, null, "minecraft:oak_log[axis=y]");
+
+    BlockSnapshot horizontalLog =
+        new BlockSnapshot(Material.OAK_LOG, 5, 70, 12, null, "minecraft:oak_log[axis=x]");
+
+    assertNotEquals(verticalLog, horizontalLog);
+  }
+
+  @Test
+  void airCanBeRepresentedExplicitly() {
+    BlockSnapshot snapshot = new BlockSnapshot(Material.AIR, 5, 70, 12, null, "minecraft:air");
+
+    assertEquals(Material.AIR, snapshot.material());
+    assertEquals("minecraft:air", snapshot.blockData());
+  }
+}

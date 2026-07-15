58a0ddf76e23c170d7476847d091128defb01800
Null_Codes <20952594+Null-Codes@users.noreply.github.com>
2026-07-15 10:41:48 -0500

    load persistent hearth state on startup

--- src/main/java/com/null_codes/hearth/HearthPlugin.java
+++ src/main/java/com/null_codes/hearth/HearthPlugin.java
@@ -4,6 +4,7 @@ import com.null_codes.hearth.command.HearthCommand;
 import com.null_codes.hearth.listener.PropertyEventListener;
 import com.null_codes.hearth.service.PropertyChangeManager;
 import com.null_codes.hearth.service.PropertyManager;
+import com.null_codes.hearth.storage.SqliteHearthStore;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,12 +14,17 @@ public class HearthPlugin extends JavaPlugin {
 
   private PropertyManager propertyManager;
   private PropertyChangeManager propertyChangeManager;
+  private SqliteHearthStore store;
 
   @Override
   public void onEnable() {
 
-    propertyManager = new PropertyManager();
-    propertyChangeManager = new PropertyChangeManager();
+    if (!getDataFolder().exists() && !getDataFolder().mkdirs()) {
+      throw new IllegalStateException("Could not create the Hearth data directory.");
+    }
+    store = new SqliteHearthStore(getDataFolder().toPath().resolve("hearth.db"));
+    propertyManager = new PropertyManager(store);
+    propertyChangeManager = new PropertyChangeManager(store);
 
     getLifecycleManager()
         .registerEventHandler(
@@ -33,5 +39,10 @@ public class HearthPlugin extends JavaPlugin {
 
     PluginManager pm = getServer().getPluginManager();
     pm.registerEvents(new PropertyEventListener(propertyManager, propertyChangeManager), this);
+  }
+
+  @Override
+  public void onDisable() {
+    if (store != null) store.close();
   }
 }
